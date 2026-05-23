22d374a61f9a691eb17d8a2a3e4b849d177e30b3
ngandjui <ivanngandjui22@gmail.com>
2026-05-23 13:22:18 +0100

    pkfrc://premier commit

---
+++ src/main/java/com/pkfrc/rdvservice/RdvserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pkfrc.rdvservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdvserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdvserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pkfrc/rdvservice/RdvserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkfrc.rdvservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdvserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
