8d5f1d4c27dfdacc52bd3431018d20b58f6aedb8
ngandjui <ivanngandjui22@gmail.com>
2026-05-24 03:07:25 +0100

    Services Entity

---
+++ src/main/java/com/pkfrc/rdvservice/entity/Services.java
@@ -0,0 +1,22 @@
+package com.pkfrc.rdvservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Entity
+@Table(name = "service")
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Getter
+@Setter
+@SuperBuilder
+public class Services extends Abstract{
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NomService nom;
+    @OneToMany(mappedBy = "service", cascade = CascadeType.ALL)
+    private List<RendezVous> rdvList;
+}
