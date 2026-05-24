17a538018333b2c39acdcd83bef4ba5607f5b9fe
ngandjui <ivanngandjui22@gmail.com>
2026-05-24 03:07:41 +0100

    Services Repository

---
+++ src/main/java/com/pkfrc/rdvservice/repository/ServiceRepository.java
@@ -0,0 +1,16 @@
+package com.pkfrc.rdvservice.repository;
+
+import com.pkfrc.rdvservice.entity.NomService;
+import com.pkfrc.rdvservice.entity.Services;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Services, Long> {
+    Optional<Services> findByIdAndIsDeletedFalse(Long id);
+    Boolean existsByNomAndIsDeletedFalse(NomService nom);
+    List<Services> findAllByIsDeletedFalse();
+}
